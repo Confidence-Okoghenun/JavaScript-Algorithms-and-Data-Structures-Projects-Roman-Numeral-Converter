8e13b1dcbae0748d4e70d56c882316769419f266
Confidence Okoghenun <confidenceboi@gmail.com>
2019-01-02 22:22:43 +0100

    chore: Initialised repo with basic code

---
+++ main.js
@@ -0,0 +1,19 @@
+function convertToRoman(num) {
+  let one = 1,
+      five = 5,
+      ten = 10,
+      fifty = 50,
+      hundred = 100,
+      fivehundred = 500,
+      thousand = 1000;
+  if(num < five) {
+
+  } else if(num < ten) {
+
+  } else if(num < fifty) {
+      
+  }
+return num;
+}
+
+console.log(convertToRoman(36));
